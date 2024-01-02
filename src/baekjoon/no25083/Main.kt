1f192f53429265e273f28ed794c03c411090b3a3
seongkyu-lim <96x60812@gmail.com>
2024-01-02 23:51:27 +0900

    Feat: 25083

---
+++ src/baekjoon/no25083/Main.kt
@@ -0,0 +1,13 @@
+
+class Main {
+
+}
+
+fun main() {
+    println("         ,r\'\"7")
+    print("r`-_   ,'  ,/\n")
+    print(" \\. \". L_r'\n")
+    print("   `~\\/\n")
+    print("      |\n")
+    print("      |\n")
+}
