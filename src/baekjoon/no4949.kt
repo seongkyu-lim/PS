9dbd0ca1e9b503ba990542b95886c2c84b24fa03
seongkyu-lim <96x60812@gmail.com>
2024-05-21 14:59:20 +0900

    feat: no4949 ing

--- src/baekjoon/no4949.kt
+++ src/baekjoon/no4949.kt
@@ -1,27 +1,44 @@
+
+private val arr1 = arrayOf("(", "[")
+private val arr2 = arrayOf(")", "]")
+
 fun main () {
     // stack에 (, [, { 가 있으면 적제하고, }]) 가 나올 때 stack에서 꺼내서 매칭이되는지 체크.
-
-    const arr1 = ["(", "[", "{"]
-    const arr2 = [")", "]", "}"]
     while(true) {
-        var input = readLine()!!
-        if(input.length == 0 and input[0] = '.') break
-        print(check(input))
+        val input = readLine()!!
+        // println("input.length : ${input.length} / input.contains('.') : ${input.contains('.')}")
+        if(input.length == 1 && input.contains('.')) break
+        println(check(input))
     }
 }
 
-fun check(input: string): string {
-        val deque = ArrayDeque<Char>()
+fun check(input: String): String {
+        // println("========== check ===========")
+
+        val deque = ArrayDeque<String>()
         for (value in input) {
             // println(value)
-            if (value in arr1) deque.addLast(value)
-            if (value in arr2) {
-                when (value) {
-                    ')' -> if(dqeue.last() !== '(') return "no"
-                    '}' -> if(deque.last() !== '{') return "no"
-                    ']' -> if(deque.last() !== '[') return "no"
+            if (value.toString() in arr1) deque.addLast(value.toString())
+            if (value.toString() in arr2) {
+                // print( "deque.last() : ${deque.last()} / ${deque.last() !== "("}" )
+                if (deque.isEmpty()) return "no"
+                when (value.toString()) {
+                    ")" -> if(deque.last() != "(") return "no"
+                    "}" -> if(deque.last() != "{") return "no"
+                    "]" -> if(deque.last() != "[") return "no"
                 }
                 deque.removeLast()
             }
-                    return "yes"
-        },
+            // println("deque : ${deque}")
+        }
+        // println("========== check finish ===========")
+        return "yes"
+}
+
+/*
+반례
+
+([)].
+.
+
+ */
